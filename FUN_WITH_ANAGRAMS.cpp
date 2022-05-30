62d942add6c7abdce3abb39f48d18aeb8bbb07bf
Elvis David Minaya Mamani <eminayama@ulasalle.edu.pe>
2022-05-30 17:04:13 -0500

    Solution

--- FUN_WITH_ANAGRAMS.cpp
+++ FUN_WITH_ANAGRAMS.cpp
@@ -2,53 +2,94 @@
 
 using namespace std;
 
-/*
- * Complete the 'shortestSubstring' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts STRING givenString as parameter.
- */
+string ltrim(const string &);
+string rtrim(const string &);
 
-int shortestSubstring(string givenString){
-    string res;
-    int n=givenString.length();
-    int dist_count=0;
-    unordered_map<int,int> hash_map;
-    for(int i=0;i<n;i++) hash_map[givenString[i]]++;
-    dist_count = hash_map.size();
-    int size = INT_MAX;
-    for(int i=0;i<n;i++) {
-        int count=0;
-        int visited[256]={0};
-        string sub_str="";
-        for(int j=i;j<n;j++) {
-            if(visited[givenString[j]]==0){
-                count+a+;
-                visited[givenString[j]]=1;
-            }
-            sub_str+=givenString[j];
-            if(count==dist_count) break;
-        }
-        if(sub_str.length()<size && count==dist_count){
-            res=sub_str;
-            size=res.length();
-        }
-    }
-    return res.size();
+
+
+/*
+ * Complete the 'funWithAnagrams' function below.
+ *
+ * The function is expected to return a STRING_ARRAY.
+ * The function accepts STRING_ARRAY text as parameter.
+ */
+bool areAnagram(string str1, string str2){
+    int n1=str1.length();
+    int n2=str2.length();
+    if(n1!=n2) return false;
+    sort(str1.begin(), str1.end());
+    sort(str2.begin(), str2.end());
+    for(int i=0;i<n1;i++) if(str1[i]!=str2[i]) return false;
+    return true;
 }
 
-int main(){
+vector<string> funWithAnagrams(vector<string> text){
+    for(int i=0;i<text.size();i++){
+        for(int j=i+1;j<text.size();j++){        
+            if(areAnagram(text[i],text[j])){
+                text.erase(text.begin()+j);  
+                j-=1;   
+            }
+        }
+    }
+    sort(text.begin(),text.end());
+    return text;
+}
+
+int main()
+{
     ofstream fout(getenv("OUTPUT_PATH"));
 
-    string givenString;
-    getline(cin, givenString);
+    string text_count_temp;
+    getline(cin, text_count_temp);
 
-    int result = shortestSubstring(givenString);
+    int text_count = stoi(ltrim(rtrim(text_count_temp)));
 
-    fout << result << "\n";
+    vector<string> text(text_count);
+
+    for (int i = 0; i < text_count; i++) {
+        string text_item;
+        getline(cin, text_item);
+
+        text[i] = text_item;
+    }
+
+    vector<string> result = funWithAnagrams(text);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
 
     fout.close();
 
     return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
 
